20427df8853594c0b8add70e8feff4177530d829
xprilion <xprilion@gmail.com>
2024-01-09 20:59:52 +0530

    init :rocket:

---
+++ src/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CallsApi';
+export * from './CampaignsApi';
+export * from './ListsApi';
+export * from './PhoneNumbersApi';
+export * from './SubscribersApi';
+export * from './SupervisorsApi';
+export * from './WebhooksApi';
